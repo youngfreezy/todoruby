dc25ba9ee4b21ce533c95f7e6998077e6a6aa253
Fareez Ahmed <Fareez.Ahmed@gmail.com>
2015-03-10 11:39:58 -0400

    validations 2

--- Gemfile
+++ Gemfile
@@ -33,7 +33,7 @@ group :doc do
 end
 
 group :development, :test do
-	gem 'rspec-rails', '~> 2.0'
+	gem 'rspec-rails', '<3'
 end
 
 group :test do
--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,2 +1,5 @@
 class TodoList < ActiveRecord::Base
+
+validates :title,       presence: true, length: { minimum: 3 }
+validates :description, presence: true, length: { minimum: 5 }
 end
