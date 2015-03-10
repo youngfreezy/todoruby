1fd4a51cf1e59972a8cdabc2ed14545165614548
Fareez Ahmed <Fareez.Ahmed@gmail.com>
2015-03-10 10:50:19 -0400

    Generated Todo Scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Odot::Application.routes.draw do
+  resources :todo_lists
+  root 'todo_lists#index' 
+  # ^doing exactly what it says on line 8
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
